191c70bedd6d10c35f01016d5698fa8e21b3b07b
Aijaz Baig <aijazbaig1@gmail.com>
2023-02-17 11:35:37 +0530

    Enhanced the class by adding a truck subclass and renaming original class to vehicle

--- ch10-primer-on-oop/class-truck.py
+++ ch10-primer-on-oop/class-truck.py
@@ -2,7 +2,7 @@
 import datetime
 import pdb
 
-class truck:
+class vehicle:
 
     species = "Motor vehicles"
 
@@ -29,13 +29,14 @@ class truck:
     def GetMileage(self):
         return self.Mileage
 
-    def GetSellingprice(self):
+    def GetSellingPrice(self):
         return self.SellingPrice
 
     def GetRegistrationYear(self):
         return self.RegistrationYr
 
-Truck1 = truck("Ford", "F-150", "California 921553")
+'''
+Truck1 = vehicle("Ford", "F-150", "California 921553")
 
 print(Truck1)
 
@@ -50,6 +51,7 @@ newline = '\n'
 print(f"User asks ${Truck1.SellingPrice} for his {Truck1.make} {Truck1.model} with " \
         f"{Truck1.registration.split()[0]} plates{newline}"
         f"It has {Truck1.Mileage} miles on it and was registered in {Truck1.RegistrationYr}");
+'''
 
 def CheckFairPrice(vehicle):
     #pdb.set_trace()
@@ -73,4 +75,48 @@ def CheckFairPrice(vehicle):
         else:
             print("price fairness not evaluated for now")
         
-CheckFairPrice(Truck1)
+
+class truck(vehicle):
+    # __repr__ derives from __str__ hence it must *return* a string, NOT print it
+    def __repr__(self):
+        newline = '\n'
+        (NumCyl, HP, Torque) = self.GetEngineDetails()
+        if (NumCyl > 0):
+            EngineDetails = f"It has a {NumCyl} cylinder engine with {HP}hp and torque value of {Torque}lb-ft"
+        else:
+            EngineDetails = ""
+        return (f"Truck registered as \"{self.registration}\" is a {self.make} {self.model}{newline}" + EngineDetails)
+
+    def SetEngineDetails(self):
+        self.NumCylinders = int(input("How many cylinders does this truck have?: "))
+        self.HP = int(input("What is the engine capacity in horsepower?: "))
+        self.Torque = int(input("What is the engine's torque in lb-ft?: "))
+        self.RPM = (self.HP * 5252)/(self.Torque)
+
+    def SetTypicalUsage(self):
+        IsHauling = str(input("Was it typically used for hauling? yes/no: "))
+        self.IsHauling = True if ('yes' in IsHauling) else False
+
+    def GetEngineDetails(self):
+        try:
+            NumCyl = self.NumCylinders
+            HP = self.HP
+            Torque = self.Torque
+            return (NumCyl, HP, Torque)
+        except AttributeError:
+            return (0,0,0)
+
+print("\n\n/************************************************/")
+Truck2 = truck("Dodge", "RAM", "California 921606")
+
+print(Truck2)
+
+Truck2.SetSellingPrice()
+Truck2.SetMileage()
+Truck2.SetRegistrationYear()
+
+Truck2.SetEngineDetails()
+Truck2.SetTypicalUsage()
+print(Truck2)
+
+CheckFairPrice(Truck2)
